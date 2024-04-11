fda94db91314e0520f8557332775edbd0323bff5
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2024-04-11 23:43:49 +0100

    Updated the about section.

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -9,7 +9,7 @@ function About() {
         <img src={iyanuImg} className={style.image} />
       </div>
       <div className={style.right}>
-        <h3>About Me</h3>
+        <h3>About <span>Me</span></h3>
         <p>
           I am a young software engineer with experience in both
           backend and frontend web development. I have an innate
@@ -17,6 +17,7 @@ function About() {
           minded and keen to learn new things. Excited to get my feet
           on the ground in the world of tech and making an impact.
         </p>
+        <button>Download CV</button>
       </div>
     </section>
   )
