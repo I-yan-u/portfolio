cd32cb9d15933f950775e7a86585bab137c7eb6d
I-yan-u <iyanuajimobi12@gmail.com>
2024-04-18 00:21:49 +0100

    Add nodemailer and @types/nodemailer as dependencies

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,18 +1,33 @@
 // import React from 'react'
 import style from '../css/Contact.module.css'
 import { Twitter, Linkedln, Github } from '../assets'
+import { useState } from 'react';
 
 function Footer() {
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
+  const sendEmail = async (sender, subject, text) => {
+    return [sender, subject, text];
+  }
+
+  const SendMail = () => {
+    console.log(email, subject, message);
+    const [status, response] = sendEmail(email, subject, message);
+    console.log(status, response);
+  }
+
   return (
     <section className={style.container} id="Contact">
       <footer className={style.footer}>
         <div className={style.form}>
           <p>Get In Touch</p>
           <input type="text" placeholder='Your Name'/>
-          <input type="email" placeholder='Your Email'/>
-          <input type="text" placeholder='Your Subject'/>
-          <textarea rows={5} placeholder='Enter your message'></textarea>
-          <button>Submit</button>
+          <input onChange={e => setEmail(e.target.value)} type="email" placeholder='Your Email'/>
+          <input onChange={e => setSubject(e.target.value)} type="text" placeholder='Your Subject'/>
+          <textarea rows={5} onChange={e => setMessage(e.target.value)} placeholder='Enter your message'></textarea>
+          <button onClick={SendMail}>Submit</button>
         </div>
         <div className={style.contacts}>
           <h3>Contacts</h3>
